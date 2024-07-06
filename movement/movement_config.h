4c4bc9fc9bf73faf053ce8780c6253128c806611
Niko Yasui <yasuiniko@gmail.com>
2024-07-06 02:32:34 -0600

    swapped advanced clock for simple clock and removed preferences

--- movement/movement_config.h
+++ movement/movement_config.h
@@ -28,14 +28,13 @@
 #include "movement_faces.h"
 
 const watch_face_t watch_faces[] = {
-    simple_clock_face,
+    clock_face,
     timer_face,
     simple_coin_flip_face,
     tomato_face,
     couch_to_5k_face,
     wake_face,
     
-    preferences_face,
     set_time_face,
     finetune_face,
     nanosec_face,
@@ -51,7 +50,7 @@ const watch_face_t watch_faces[] = {
  * Some folks also like to use this to hide the preferences and time set faces from the normal rotation.
  * If you don't want any faces to be excluded, set this to 0 and a long Mode press will have no effect.
  */
-#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 6) // or (0)
+#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 5) // or (0)
 
 /* Custom hourly chime tune. Check movement_custom_signal_tunes.h for options. */
 #define SIGNAL_TUNE_DEFAULT
